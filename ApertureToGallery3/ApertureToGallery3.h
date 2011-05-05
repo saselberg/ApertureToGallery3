a134585b6f765d9ecaf6c86c87edf84469495f37
Scott Selberg <saselberg@gmail.com>
2011-05-05 00:23:43 -0700

    Added in Gallery3 Browser

--- ApertureToGallery3/ApertureToGallery3.h
+++ ApertureToGallery3/ApertureToGallery3.h
@@ -10,6 +10,10 @@
 #import <Foundation/Foundation.h>
 #import "ApertureExportManager.h"
 #import "ApertureExportPlugIn.h"
+#import "GalleryDirectoryController.h"
+#import "GalleryAlbum.h"
+#import "GalleryInfo.h"
+#import "RestfulGallery.h"
 
 @interface ApertureToGallery3 : NSObject <ApertureExportPlugIn> 
 {
@@ -33,6 +37,48 @@
         IBOutlet NSView *settingsView;
         IBOutlet NSView *firstView;
         IBOutlet NSView *lastView;
+    
+    IBOutlet NSTextField *albumName;
+    IBOutlet NSTextField *albumTitle;
+    IBOutlet NSTextField *newGalleryPassword;
+    
+    IBOutlet NSBrowser                  *browser;
+    IBOutlet NSTableView                *galleryDirectoryTableView;    
+    IBOutlet GalleryDirectoryController *galleryDirectoryController;
+
+    IBOutlet NSWindow *manageGalleriesWindow;
+    IBOutlet NSWindow *galleryInformationWindow;
+    IBOutlet NSWindow *addAlbumWindow;
+    
+    NSMutableDictionary *preferences;
+    NSNumber            *selectedGalleryIndex;
+    NSDictionary        *userDefaults;
+    GalleryAlbum        *selectedGallery;
+
 }
+
+@property (retain) RestfulGallery   *gallery;
+@property (retain) GalleryAlbum     *rootGalleryAlbum;
+@property (retain) NSMutableArray   *galleryDirectory;
+@property (retain) NSString         *galleryApiKey;
+@property (retain) NSString         *filePath;
+
+
+-(IBAction)addPhoto:(id)sender;
+-(IBAction)makeAlbum:(id)sender;
+-(IBAction)getApiKey:(id)sender;
+
+-(IBAction)showManageGalleries:(id)sender;
+-(IBAction)hideManageGalleries:(id)sender;
+
+-(IBAction)addGalleryInformation:(id)sender;
+-(IBAction)showGalleryInformation:(id)sender;
+-(IBAction)hideGalleryInformation:(id)sender;
+
+-(IBAction)showAddAlbum:(id)sender;
+-(IBAction)hideAddAlbum:(id)sender;
+
+-(void) savePreferences;
+
 @end
 
--- ApertureToGallery3/ApertureToGallery3.m
+++ ApertureToGallery3/ApertureToGallery3.m
@@ -10,6 +10,11 @@
 
 
 @implementation ApertureToGallery3
+@synthesize gallery;
+@synthesize galleryDirectory;
+@synthesize rootGalleryAlbum;
+@synthesize galleryApiKey;
+@synthesize filePath;
 
 //---------------------------------------------------------
 // initWithAPIManager:
@@ -33,9 +38,28 @@
 		
         _progressLock = [[NSLock alloc] init];
 		
-		// Finish your initialization here
-	}
-	
+        self.gallery  = [[RestfulGallery alloc] init];        
+        userDefaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:[[NSBundle bundleForClass:[self class]] bundleIdentifier]];
+        if( userDefaults ){
+            preferences = [userDefaults mutableCopy];
+            if( [preferences objectForKey:@"GALLERY_DIRECTORY"] ){
+                self.galleryDirectory =  [[NSKeyedUnarchiver unarchiveObjectWithData:[preferences objectForKey:@"GALLERY_DIRECTORY"]] mutableCopy];                
+            } else {
+                self.galleryDirectory = [NSMutableArray arrayWithCapacity:0];                            
+            }
+            
+            if( [preferences objectForKey:@"SELECTED_GALLERY_INDEX"] ){
+                selectedGalleryIndex = [preferences objectForKey:@"SELECTED_GALLERY_INDEX"];
+            } else {
+                selectedGalleryIndex = [NSNumber numberWithInteger:0];                
+            }
+        } else {
+            preferences = [[NSMutableDictionary alloc] init];
+            self.galleryDirectory = [NSMutableArray arrayWithCapacity:5];            
+            selectedGalleryIndex = [NSNumber numberWithInteger:0];
+        }
+	}	
+    
 	return self;
 }
 
@@ -45,9 +69,18 @@
 	[_topLevelNibObjects makeObjectsPerformSelector:@selector(release)];
 	[_topLevelNibObjects release];
 	
+	
+    self.gallery                  = nil;
+    self.rootGalleryAlbum         = nil;
+    self.galleryApiKey            = nil;
+    self.galleryDirectory         = nil;
+    
+    [preferences release];
+    preferences = nil;
+
 	[_progressLock release];
 	[_exportManager release];
-	
+    
 	[super dealloc];
 }
 
@@ -63,12 +96,17 @@
 		// Load the nib using NSNib, and retain the array of top-level objects so we can release
 		// them properly in dealloc
 		NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
-		NSNib *myNib = [[NSNib alloc] initWithNibNamed:@"BundleTest" bundle:myBundle];
+		NSNib *myNib = [[NSNib alloc] initWithNibNamed:@"ApertureToGallery3" bundle:myBundle];
 		if ([myNib instantiateNibWithOwner:self topLevelObjects:&_topLevelNibObjects])
 		{
 			[_topLevelNibObjects retain];
+            [galleryDirectoryController setSelectionIndex:[selectedGalleryIndex integerValue]];
+            selectedGallery            = [[galleryDirectoryController selectedObjects] objectAtIndex:0];
+            self.gallery.galleryApiKey = selectedGallery.key;
+            self.gallery.url           = selectedGallery.url;
+
 		}
-        [myNib release];
+        [myNib release];        
 	}
 	
 	return settingsView;
@@ -224,4 +262,191 @@
 	[_progressLock unlock];
 }
 
+/************************************************************
+ / Gallery actions
+ ************************************************************/
+
+- (IBAction)addPhoto:(id)sender
+{
+    GalleryAlbum *selectedAlbum;
+    selectedAlbum = (GalleryAlbum *)[browser itemAtIndexPath:[browser selectionIndexPath]];
+    
+    [gallery addPhotosAtPath:self.filePath toUrl:selectedAlbum.url];
+}
+
+- (IBAction)makeAlbum:(id)sender
+{
+    NSNumber *localEntityId;
+    NSNumber *newColumn;
+    NSString *newAlbumUrl;
+    NSArray  *albumChildren;
+    GalleryAlbum *selectedAlbum;
+    
+    selectedAlbum = (GalleryAlbum *)[browser itemAtIndexPath:[browser selectionIndexPath]];
+    
+    if( selectedAlbum == nil ){
+        [browser selectRow:0 inColumn:0];
+        selectedAlbum = (GalleryAlbum *)[browser itemAtIndexPath:[browser selectionIndexPath]];
+    }
+    
+    localEntityId = [selectedAlbum entityId];
+    
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithCapacity:4];
+    [parameters setObject:[albumName  stringValue] forKey:@"name"];
+    [parameters setObject:[albumTitle stringValue] forKey:@"title"];
+    
+    [gallery createAlbumInEntity:localEntityId withParameters:parameters];
+    newAlbumUrl= [[gallery results] objectForKey:@"url"];
+    
+    selectedAlbum.dataIsStale      = true;
+    selectedAlbum.childrenAreStale = true;        
+    newColumn = [NSNumber numberWithInteger:([browser selectedColumn]+1)];
+    albumChildren = [selectedAlbum children];
+    
+    for (NSInteger col = [browser lastColumn]; col >= 0; col--) {
+        [browser reloadColumn:col];
+    }
+    
+    if( [browser lastColumn] < [newColumn integerValue] )
+    {
+        [browser addColumn];
+        [browser scrollColumnsLeftBy:1];
+    }
+    
+    for( int i = 0; i < [albumChildren count]; i++ )
+    {
+        GalleryAlbum *album = (GalleryAlbum *)[browser itemAtRow:i inColumn:[newColumn integerValue]];
+        NSLog( @"%d:%@ => %@", i, newColumn, album.url );
+        if( [newAlbumUrl isEqualToString:[album url]] )
+        {
+            [browser selectRow:i inColumn:[newColumn integerValue]];
+            continue;
+        }
+    }
+    
+    // clear the text fields
+    [albumName  setStringValue:@""];
+    [albumTitle setStringValue:@""];
+    
+    [addAlbumWindow orderOut:nil];
+    [NSApp endSheet:addAlbumWindow];     
+}
+
+- (IBAction) getApiKey:(id)sender
+{
+    GalleryInfo *galleryInfo = [[galleryDirectoryController selectedObjects] objectAtIndex:0];
+    [gallery getApiKeyforGallery:galleryInfo.url AndUsername:galleryInfo.username AndPassword:[newGalleryPassword stringValue]];
+    galleryInfo.key = [gallery.results objectForKey:@"GALLERY_RESPONSE"];
+    
+    [NSAlert alertWithMessageText:[NSString stringWithFormat:@"Key %@", galleryInfo.key] defaultButton:@"OK" alternateButton:@"Also OK" otherButton:@"Yup" informativeTextWithFormat:@"informative text"];
+}
+
+/************************************************************
+ /  Manage window sheets
+ ************************************************************/
+-(IBAction)showManageGalleries:(id)sender
+{
+    [NSApp beginSheet:manageGalleriesWindow modalForWindow:[_exportManager window] modalDelegate:self didEndSelector:NULL contextInfo:nil];    
+}
+-(IBAction)hideManageGalleries:(id)sender
+{
+    selectedGallery = [[galleryDirectoryController selectedObjects] objectAtIndex:0];
+    self.gallery.galleryApiKey = selectedGallery.key;
+    self.gallery.url           = selectedGallery.url;
+    
+    if( ![selectedGalleryIndex isEqualToNumber:[NSNumber numberWithInteger:[galleryDirectoryController selectionIndex]]] )
+    {
+        rootGalleryAlbum.dataIsStale      = true;
+        rootGalleryAlbum.childrenAreStale = true;        
+        
+        for (NSInteger col = [browser lastColumn]; col >= 0; col--) {
+            [browser reloadColumn:col];
+        }
+    }
+    
+    selectedGalleryIndex = [NSNumber numberWithInteger:[galleryDirectoryController selectionIndex]];
+    [self savePreferences];
+    
+    [manageGalleriesWindow orderOut:nil];
+    [NSApp endSheet:manageGalleriesWindow];     
+}
+
+-(IBAction)addGalleryInformation:(id)sender
+{
+    [galleryDirectoryController add:self];
+    [NSApp beginSheet:galleryInformationWindow modalForWindow:manageGalleriesWindow modalDelegate:self didEndSelector:NULL contextInfo:nil];    
+}
+-(IBAction)showGalleryInformation:(id)sender
+{
+    [NSApp beginSheet:galleryInformationWindow modalForWindow:manageGalleriesWindow modalDelegate:self didEndSelector:NULL contextInfo:nil];    
+}
+-(IBAction)hideGalleryInformation:(id)sender
+{
+    [newGalleryPassword setStringValue:@""];
+    
+    [galleryInformationWindow orderOut:nil];
+    [NSApp endSheet:galleryInformationWindow];     
+}
+
+-(IBAction)showAddAlbum:(id)sender
+{
+    [NSApp beginSheet:addAlbumWindow modalForWindow:[_exportManager window] modalDelegate:self didEndSelector:NULL contextInfo:nil];    
+}
+-(IBAction)hideAddAlbum:(id)sender
+{
+    [addAlbumWindow orderOut:nil];
+    [NSApp endSheet:addAlbumWindow];     
+}
+
+/************************************************************
+ /  Methods to enable the browser
+ ************************************************************/
+
+
+- (id)rootItemForBrowser:(NSBrowser *)browser
+{
+#pragma unused (browser)
+//        NSLog( @"rootItemForBrowser" );
+    if (rootGalleryAlbum == nil) {
+        rootGalleryAlbum = [[GalleryAlbum alloc] initWithGallery:gallery andEntityId:[NSNumber numberWithInteger:0]];
+    }
+    return rootGalleryAlbum;    
+}
+
+- (NSInteger)browser:(NSBrowser *)browser numberOfChildrenOfItem:(id)item 
+{
+#pragma unused (browser)
+//        NSLog( @"browser:numberOfChidrenOfItem" );
+    GalleryAlbum *album = (GalleryAlbum *)item;
+    return [album numberOfChildren];
+}
+
+- (id)browser:(NSBrowser *)browser child:(NSInteger)index ofItem:(id)item {
+#pragma unused (browser)
+//        NSLog( @"browser:child:index:ofItem" );
+    GalleryAlbum *album = (GalleryAlbum *)item;
+    return [album.children objectAtIndex:index];
+}
+
+- (BOOL)browser:(NSBrowser *)browser isLeafItem:(id)item {
+#pragma unused (browser)
+//        NSLog( @"browser:isLeafItem" );
+    GalleryAlbum *album = (GalleryAlbum *)item;
+    return !album.hasChildren;
+}
+
+- (id)browser:(NSBrowser *)browser objectValueForItem:(id)item {
+#pragma unused (browser)
+//        NSLog( @"objectValueForItem" );
+    GalleryAlbum *album = (GalleryAlbum *)item;
+    return album.displayName;
+}
+
+- (void)savePreferences {
+    [preferences setObject:[NSKeyedArchiver archivedDataWithRootObject:galleryDirectory] forKey:@"GALLERY_DIRECTORY"];    
+    [preferences setObject:selectedGalleryIndex forKey:@"SELECTED_GALLERY_INDEX"];
+    
+    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle bundleForClass: [self class]] bundleIdentifier]];
+    [[NSUserDefaults standardUserDefaults] setPersistentDomain:preferences forName:[[NSBundle bundleForClass:[self class]] bundleIdentifier]];
+}
 @end
